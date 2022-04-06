ccd238e60e6f0720329c09a0b9fd4778a334826b
Omar Bowen <bronxkidd95@gmail.com>
2022-04-06 15:17:51 -0700

    fixed indentation

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,9 +4,9 @@ class NotFound extends Component {
   render () {
     return (
       <div>
-      <div class="img3">
-        <img src="https://c8.alamy.com/zooms/9/d4f9b47ecab74f9aa14d27950751d5ca/tr609x.jpg" height="auto" width="auto"  />
-      </div>
+        <div class="img3">
+          <img src="https://c8.alamy.com/zooms/9/d4f9b47ecab74f9aa14d27950751d5ca/tr609x.jpg" height="auto" width="auto"  />
+        </div>
       </div>
     )
   }
