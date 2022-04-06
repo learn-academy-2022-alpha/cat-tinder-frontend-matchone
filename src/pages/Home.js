9f830420fef22bffbb9e8a003ebc4ba8f9f2ba5b
Omar Bowen <bronxkidd95@gmail.com>
2022-04-06 12:44:10 -0700

    checked out a branch with the correct naming conventions and pushed up last bit of changes with the notfound.js file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,11 @@ import React, { Component } from 'react'
 
 class Home extends Component {
   render () {
-    
-    return ( 
+
+    return (
     <div>
-      <div class="img2"><img src="https://i.pinimg.com/564x/a6/65/8d/a6658deaa679285cf2a3aed4d8c2cb06.jpg" height="auto" width="auto"  />
+      <div class="img2">
+        <img src="https://i.pinimg.com/564x/a6/65/8d/a6658deaa679285cf2a3aed4d8c2cb06.jpg" height="auto" width="auto"  />
       </div>
 
     </div>
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 class NotFound extends Component {
   render () {
     return (
-      <div>Footer</div>
+      <div>
+      <div class="img3">
+        <img src="https://c8.alamy.com/zooms/9/d4f9b47ecab74f9aa14d27950751d5ca/tr609x.jpg" height="auto" width="auto"  />
+      </div>
+      </div>
     )
   }
 }
