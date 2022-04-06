b2d99b0d7ca6ec9e9844ea0e565b472b9b78e4fe
Carlos Ramos <99694226+CarRamos@users.noreply.github.com>
2022-04-06 11:01:59 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,10 @@ class Footer extends Component {
   render () {
     return (
      <footer>
-      <div>
-        <div class="header"><img src="https://www.clipartmax.com/png/full/146-1467402_clipart-info-peace-love-weed.png" height="100" width="auto" />      
-        </div>
+      <div class="header">
+        <img src="https://www.clipartmax.com/png/full/146-1467402_clipart-info-peace-love-weed.png" height="100" width="auto" />      
       </div>
-      </footer>
+     </footer>
     )
   }
 }
