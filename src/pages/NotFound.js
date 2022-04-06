994b69071656f145bf57ca7e67e2bd072aaceb63
Omar Bowen <bronxkidd95@gmail.com>
2022-04-06 11:44:37 -0700

    finished front-end

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 class NotFound extends Component {
   render () {
     return (
-      <div>Footer</div>
+        <div class="img2"><img src="https://c8.alamy.com/zooms/9/d4f9b47ecab74f9aa14d27950751d5ca/tr609x.jpg" height="auto" width="auto"  />
+        <h1> Not Found </h1>
+        </div>
+
+
     )
   }
 }
