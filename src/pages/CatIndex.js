b262e1fd728266371acf47655a301b37d1ed354e
CarRamos <goodsleep415@gmail.com>
2022-04-07 11:24:07 -0700

    stated the show

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import { Card, CardTitle, Col } from 'reactstrap'
 
 class CatIndex extends Component {
   render () {
     return (
-      <div>Footer</div>
+      <>
+        <h3>Meet the Cats!</h3>
+        <br />
+        <Col sm="6">
+          {this.props.cats.map(cat => {
+            return(
+              <Card body key={cat.id}>
+                <CardTitle>
+                  <h4>{cat.name}</h4>
+                </CardTitle>
+              </Card>
+            )
+          })
+        }
+       </Col>
+      </>
     )
   }
 }
+
+
+
+
 export default CatIndex
--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react'
 
 class CatShow extends Component {
   render () {
+    console.log(this.props.cat)
     return (
-      <div>Footer</div>
+      <>
+      <h3>
+        One Cat
+      </h3>
+      <Col sm="6">
+        <Card body>
+          <CardTitle>Hi, my name is {this.props.cat.name}!</CardTitle>
+          <img src={this.props.cat.image} alt="adorable cat" />
+          <CardText>I am {this.props.cat.age} years old. I enjoy  {this.props.cat.enjoys}.</CardText>
+        </Card>
+      </Col>
+      </>
     )
   }
 }
