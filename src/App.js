405b6d664535ef9efcc4c5c7907adfe7f295e64c
Omar Bowen <bronxkidd95@gmail.com>
2022-04-11 20:24:26 -0700

    Finished Fetch

--- src/App.js
+++ src/App.js
@@ -14,66 +14,66 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
   class App extends Component {
       constructor(props){
         super(props)
-        this.state = {
-          cats: []
-        }
-      }
+          this.state = {
+            cats: []
+          }
+          }
 
-      componentDidMount(){
-        this.readCat()
-            }
+        componentDidMount(){
+          this.readCat()
+              }
 
-            createCat = (newCat) => {
-              fetch("http://localhost:3000/cats", {
-                body: JSON.stringify(newCat),
-                headers: {
-                "Content-Type": "application/json"
-                },
-                method: "POST"
-                })
+              createCat = (newCat) => {
+                fetch("http://localhost:3000/cats", {
+                  body: JSON.stringify(newCat),
+                  headers: {
+                  "Content-Type": "application/json"
+                  },
+                  method: "POST"
+                  })
+                    .then(response => response.json())
+                    .then(payload => this.readCat())
+                    .catch(errors => console.log("Cat create errors:", errors))
+                  }
+              readCat = () => {
+                fetch("http://localhost:3000/cats")
                   .then(response => response.json())
-                  .then(payload => this.readCat())
-                  .catch(errors => console.log("Cat create errors:", errors))
+                  .then(payload => this.setState({cats: payload}))
+                  .catch(errors => console.log("Cat read errors", errors))
                 }
-            readCat = () => {
-              fetch("http://localhost:3000/cats")
-                .then(response => response.json())
-                .then(payload => this.setState({cats: payload}))
-                .catch(errors => console.log("Cat read errors", errors))
+              updatedCat = (updatedCat, id) => {
+                console.log("Cat was updated", updatedCat)
+                console.log("Cat id is", id )
               }
-            updatedCat = (updatedCat, id) => {
-              console.log("Cat was updated", updatedCat)
-              console.log("Cat id is", id )
-            }
 
-            deletedCat = (id) => {
-              console.log(`Cat at ${id} was deleted`, id)
-            }
+              deletedCat = (id) => {
+                console.log(`Cat at ${id} was deleted`, id)
+              }
 
-              render() {
-                console.log(this.state)
-                return (
-                  <>
-                  <h1>Welcome to Our App!</h1>
-                  <Router>
-                    <Header />
-                      <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route path="/catindex" render={(props) => <CatIndex cats={this.state.cats} />} />
-                        <Route path="/catshow/:id" render={(props) => {
-                          let id = props.match.params.id
-                          let cat = this.state.cats.find(cat => cat.id === +id)
-                          return <CatShow cat={cat} />
-                          }} />
-                        <Route path="/catnew" render={(props) => <CatNew createCat={this.createCat} />} />
-                        <Route path="/catedit" component={CatEdit} />
-                        <Route component={NotFound}/>
-                    </Switch>
-                    <Footer />
-                </Router>
-                  </>
-                  )
+                render() {
+                  console.log(this.state)
+                  return (
+                    <>
+                    <h1>Welcome to Our App!</h1>
+                    <Router>
+                      <Header />
+                        <Switch>
+                          <Route exact path="/" component={Home} />
+                          <Route path="/catindex" render={(props) => <CatIndex cats={this.state.cats} />} />
+                          <Route path="/catshow/:id" render={(props) => {
+                            let id = props.match.params.id
+                            let cat = this.state.cats.find(cat => cat.id === +id)
+                            return <CatShow cat={cat} />
+                            }} />
+                          <Route path="/catnew" render={(props) => <CatNew createCat={this.createCat} />} />
+                          <Route path="/catedit" component={CatEdit} />
+                          <Route component={NotFound}/>
+                      </Switch>
+                      <Footer />
+                  </Router>
+                    </>
+                    )
+                  }
                 }
-              }
 
 export default App;
